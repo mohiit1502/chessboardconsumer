0891fdf01bc2d5902657344be1e40d70cd86bce5
vaasu <mohiit1502@gmail.com>
2020-04-02 19:42:55 +0530

    Updates

---
+++ precache-manifest.6bbd4cfa49ecc19dcbe5f922c66eb763.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98abfa7e3e17bde1131fbdfdca82ff40",
+    "url": "/chessboardconsumer/index.html"
+  },
+  {
+    "revision": "0bba52431f83e31a6333",
+    "url": "/chessboardconsumer/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1983a0385442ae7054a0",
+    "url": "/chessboardconsumer/static/css/3.b586e690.chunk.css"
+  },
+  {
+    "revision": "881c1c568231bc626e39",
+    "url": "/chessboardconsumer/static/css/4.4467dc84.chunk.css"
+  },
+  {
+    "revision": "912eab6228174109e618",
+    "url": "/chessboardconsumer/static/css/main.6367b37e.chunk.css"
+  },
+  {
+    "revision": "0bba52431f83e31a6333",
+    "url": "/chessboardconsumer/static/js/2.263ad055.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/chessboardconsumer/static/js/2.263ad055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1983a0385442ae7054a0",
+    "url": "/chessboardconsumer/static/js/3.0a96e1a7.chunk.js"
+  },
+  {
+    "revision": "881c1c568231bc626e39",
+    "url": "/chessboardconsumer/static/js/4.ab6c7ed9.chunk.js"
+  },
+  {
+    "revision": "86d7b1a182a1011db279",
+    "url": "/chessboardconsumer/static/js/5.0d42efe7.chunk.js"
+  },
+  {
+    "revision": "aa5100e8f5bb6b6644e4c12ecb4c755a",
+    "url": "/chessboardconsumer/static/js/5.0d42efe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "912eab6228174109e618",
+    "url": "/chessboardconsumer/static/js/main.cf934b85.chunk.js"
+  },
+  {
+    "revision": "3b083ac46309932d9d1a",
+    "url": "/chessboardconsumer/static/js/runtime-main.e95d6672.js"
+  }
+]);
